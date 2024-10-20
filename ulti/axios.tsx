25896acd24d2c872b57858fab55b69d91486b1bd
HectorVu <hung.vm2574@gmail.com>
2024-10-20 16:04:11 +0800

    Fix accidental commit

--- ulti/axios.tsx
+++ ulti/axios.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://localhost:8080",
-  // baseURL: "https://big-umbrella-c5c3450b8837.herokuapp.com/",
+  // baseURL: "http://localhost:8080",
+  baseURL: "https://big-umbrella-c5c3450b8837.herokuapp.com/",
 });
