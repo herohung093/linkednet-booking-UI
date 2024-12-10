d37e5fb0a78fa9e880c0db61901983fae3c80373
HectorVu <hung.vm2574@gmail.com>
2024-12-10 18:01:02 +0800

    Add geolocation and contact links to StoreInfo component

--- components/StoreInfo.tsx
+++ components/StoreInfo.tsx
@@ -1,32 +1,46 @@
 import { StarIcon } from "@radix-ui/react-icons";
-import React from "react";
+import LocationOnRoundedIcon from "@mui/icons-material/LocationOnRounded";
+import LocalPhoneRoundedIcon from "@mui/icons-material/LocalPhoneRounded";
+import React, { useEffect, useState } from "react";
 
-import {
-  Box,
-  Typography,
-  Rating,
-  Skeleton,
-} from '@mui/material';
+import { Box, Typography, Rating, Skeleton } from "@mui/material";
 import moment from "moment";
 
 const LoadingSkeleton = () => (
   <div>
     <Skeleton variant="text" width="60%" height={40} />
-      <Skeleton variant="rectangular" width="100%" height={40} />
-      <Box display="flex" alignItems="center" gap={2}>
-        <Skeleton variant="text" width={40} height={40} />
-        <Skeleton variant="text" width="30%" height={20} />
-      </Box>
-      <Skeleton variant="text" width="40%" height={20} />
-      <Skeleton variant="text" width="20%" height={20} />
+    <Skeleton variant="rectangular" width="100%" height={40} />
+    <Box display="flex" alignItems="center" gap={2}>
+      <Skeleton variant="text" width={40} height={40} />
+      <Skeleton variant="text" width="30%" height={20} />
+    </Box>
+    <Skeleton variant="text" width="40%" height={20} />
+    <Skeleton variant="text" width="20%" height={20} />
   </div>
 );
 
 export const StoreInfo: React.FC<any> = ({ storeConfig }) => {
+  const storeAddress = storeConfig?.storeAddress;
+  const [directionUrl, setDirectionUrl] = useState<string>("");
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        const url = `https://www.google.com/maps/dir/?api=1&origin=${latitude},${longitude}&destination=${encodeURIComponent(
+          storeAddress
+        )}`;
+        setDirectionUrl(url);
+      });
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }, [storeAddress]);
+
   const checkBusinessStatus = () => {
     const currentDateTime = moment();
-    const currentDayOfWeek = currentDateTime.format('dddd').toUpperCase(); // 0 (Sunday) to 6 (Saturday)
-    const currentTime = currentDateTime
+    const currentDayOfWeek = currentDateTime.format("dddd").toUpperCase(); // 0 (Sunday) to 6 (Saturday)
+    const currentTime = currentDateTime;
 
     const currentDayBusinessHours = storeConfig?.businessHoursList.find(
       (day: any) => day.dayOfWeek === currentDayOfWeek
@@ -36,8 +50,14 @@ export const StoreInfo: React.FC<any> = ({ storeConfig }) => {
     }
 
     const { openingTime, closingTime } = currentDayBusinessHours;
-    const openingHour = moment().set({hour: parseInt(openingTime.split(":")[0]), minute: parseInt(openingTime.split(":")[1])}); ;
-    const closingHour = moment().set({hour: parseInt(closingTime.split(":")[0]), minute: parseInt(closingTime.split(":")[1])}); ;
+    const openingHour = moment().set({
+      hour: parseInt(openingTime.split(":")[0]),
+      minute: parseInt(openingTime.split(":")[1]),
+    });
+    const closingHour = moment().set({
+      hour: parseInt(closingTime.split(":")[0]),
+      minute: parseInt(closingTime.split(":")[1]),
+    });
 
     if (currentTime.isAfter(openingHour) && currentTime.isBefore(closingHour)) {
       return `Open until ${closingTime}`;
@@ -45,10 +65,10 @@ export const StoreInfo: React.FC<any> = ({ storeConfig }) => {
       return `Currently Closed, open from ${openingTime}`;
     } else {
       // Find the next opening day and time
-      let nextOpeningDay = currentDateTime.add(1, 'days').format('dddd');
+      let nextOpeningDay = currentDateTime.add(1, "days").format("dddd");
       let nextOpeningTime = storeConfig.businessHoursList.find(
-        (day: any) => day.dayOfWeek === nextOpeningDay);
-    
+        (day: any) => day.dayOfWeek === nextOpeningDay
+      );
 
       return `Closed, open on ${nextOpeningDay} at ${nextOpeningTime}`;
     }
@@ -63,7 +83,7 @@ export const StoreInfo: React.FC<any> = ({ storeConfig }) => {
   ));
 
   return (
-    <div >
+    <div>
       <Typography variant="h4" fontWeight="bold" gutterBottom>
         {storeConfig?.storeName}
       </Typography>
@@ -76,7 +96,31 @@ export const StoreInfo: React.FC<any> = ({ storeConfig }) => {
       </Box>
 
       <Typography variant="body1" color="textSecondary" gutterBottom>
-        {storeConfig?.storeAddress}
+        <a
+          href={directionUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500"
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          <LocationOnRoundedIcon
+            style={{ verticalAlign: "middle", marginRight: "8px" }}
+          />
+          {storeConfig?.storeAddress}
+        </a>
+      </Typography>
+
+      <Typography variant="body1" color="textSecondary" gutterBottom>
+        <a
+          href={`tel:${storeConfig?.storePhoneNumber}`}
+          className="text-blue-500"
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          <LocalPhoneRoundedIcon
+            style={{ verticalAlign: "middle", marginRight: "8px" }}
+          />
+          {storeConfig?.storePhoneNumber}
+        </a>
       </Typography>
 
       {/* <Typography variant="body2" color="textSecondary" gutterBottom>
